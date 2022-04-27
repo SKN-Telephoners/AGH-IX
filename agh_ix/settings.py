088c38ca3df6e0620f5812df52bdb3aee186498d
PiotrWodecki <Piotr.M.Wodecki@gmail.com>
2022-04-27 17:19:40 +0200

    Dockerize app
    Includes development docker-compose with postfix-catchall image
    It is not supposed to be used for production purposes

--- agh_ix/settings.py
+++ agh_ix/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'agh_ix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -127,7 +132,7 @@ LOGIN_URL="/login/"
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_USE_TLS = False
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'postfix'
 EMAIL_HOST_USER = 'registration@localhost'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 25
