49f6ee9ab88ff50abc62e98f2c636fa1f4e538e4
PiotrWodecki <Piotr.M.Wodecki@gmail.com>
2022-05-05 15:12:02 +0200

    Make some fields required

--- core/forms.py
+++ core/forms.py
@@ -5,6 +5,11 @@ from django.contrib.auth import get_user_model
 get_user_model()
 
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', )
