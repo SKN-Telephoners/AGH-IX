00f0e99a4efc1cc95d566b84b48e00412ef1635b
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2022-08-12 01:33:05 +0200

    Disable debug mode in flask

--- docker/zerotier/app.py
+++ docker/zerotier/app.py
@@ -80,4 +80,4 @@ shell2http.register_command(
 )
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
