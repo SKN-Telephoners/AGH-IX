91f70e210b06b6701955d89cbb0b91b8a564951a
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2022-08-11 16:49:00 +0200

    Black fixes
    formating

--- docker/zerotier/app.py
+++ docker/zerotier/app.py
@@ -13,46 +13,71 @@ users = {
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-def logging_decorator(f):
-  @functools.wraps(f)
-  def decorator(*args, **kwargs):
-    print("*" * 64)
-    print(
-      "from logging_decorator: " + request.url + " : " + str(request.remote_addr)
-    )
-    print("*" * 64)
-    return f(*args, **kwargs)
 
-  return decorator
+def logging_decorator(f):
+    @functools.wraps(f)
+    def decorator(*args, **kwargs):
+        print("*" * 64)
+        print(
+            "from logging_decorator: " + request.url + " : " + str(request.remote_addr)
+        )
+        print("*" * 64)
+        return f(*args, **kwargs)
+
+    return decorator
+
 
 def login_required(f):
-  @functools.wraps(f)
-  def decorator(*args, **kwargs):
-    user = verify_login()
-    if not user == "aghix":
-      abort(Response("You are not logged in.", 401))
-    return f(*args, **kwargs)
+    @functools.wraps(f)
+    def decorator(*args, **kwargs):
+        user = verify_login()
+        if not user == "aghix":
+            abort(Response("You are not logged in.", 401))
+        return f(*args, **kwargs)
 
-  return decorator
+    return decorator
+
 
 executor = Executor(app)
 shell2http = Shell2HTTP(app=app, executor=executor, base_url_prefix="/commands/")
 
+
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
+    if username in users and check_password_hash(users.get(username), password):
         return username
+
 
 @auth.login_required
 def verify_login():
-  return auth.username()
+    return auth.username()
 
-shell2http.register_command(endpoint="saythis", command_name="echo", decorators=[login_required, logging_decorator])
-shell2http.register_command(endpoint="brctl", command_name="brctl", decorators=[login_required, logging_decorator])
-shell2http.register_command(endpoint="ifconfig", command_name="ifconfig", decorators=[login_required, logging_decorator])
-shell2http.register_command(endpoint="zerotier", command_name="zerotier-cli", decorators=[login_required, logging_decorator])
-shell2http.register_command(endpoint="zerotierone", command_name="zerotier-one -d", decorators=[login_required, logging_decorator])
+
+shell2http.register_command(
+    endpoint="saythis",
+    command_name="echo",
+    decorators=[login_required, logging_decorator],
+)
+shell2http.register_command(
+    endpoint="brctl",
+    command_name="brctl",
+    decorators=[login_required, logging_decorator],
+)
+shell2http.register_command(
+    endpoint="ifconfig",
+    command_name="ifconfig",
+    decorators=[login_required, logging_decorator],
+)
+shell2http.register_command(
+    endpoint="zerotier",
+    command_name="zerotier-cli",
+    decorators=[login_required, logging_decorator],
+)
+shell2http.register_command(
+    endpoint="zerotierone",
+    command_name="zerotier-one -d",
+    decorators=[login_required, logging_decorator],
+)
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
